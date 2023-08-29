d504c5f8875329a997243a3276f13668950f7ec0
chickenchickenlove <ojt90902@naver.com>
2023-08-29 23:07:52 +0900

    git init

---
+++ syncronize-test/src/main/java/com/me/syncronizedtest/SyncronizedtestApplication.java
@@ -0,0 +1,13 @@
+package com.me.syncronizedtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncronizedtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncronizedtestApplication.class, args);
+	}
+
+}
---
+++ syncronize-test/src/test/java/com/me/syncronizedtest/SyncronizedtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.syncronizedtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncronizedtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
